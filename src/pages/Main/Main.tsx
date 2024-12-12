78ef6e04e9550a7fd200973893be3126c359a521
khanzele <khanzele16@gmail.com>
2024-12-12 18:04:07 +0300

    доделал файл

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import setTitle from '../../utils/setTitle'
+
+function Main() {
+	setTitle('Онлайн-школа Brain Academy')
+	return (
+		<div className='Main'>
+			
+		</div>
+	)
+}
+
+export default Main
---
+++ src/utils/setTitle.ts
@@ -0,0 +1,3 @@
+export default (title: string) => {
+	document.title = title
+}
