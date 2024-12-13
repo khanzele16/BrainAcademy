83605026a251f7b0679e8a4d1caa0456c1f0892c
webddevel <unar0448@gmail.com>
2024-12-13 11:43:43 +0300

    исправить

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import setTitle from '../../utils/setTitle'
+import logo from '../../assets/images/лого.svg'
 
 function Main() {
 	setTitle('Онлайн-школа Brain Academy')
 	return (
 		<div className='Main'>
-			
+			<header className='Header'>
+				<div className="header-container">
+					<div className="logo">
+						<img src={logo} alt="" />
+					</div>
+					<div className="navigation">
+						<ul className="menu">
+							<li className='menu-item'><a className='menu-link' href="">Курсы</a></li>
+							<li className='menu-item'><a className='menu-link' href="">Регистрация</a></li>
+
+						</ul>
+					</div>
+				</div>
+			</header>
 		</div>
 	)
 }
